efa0c6d5e2f3846cf2973a362dff9220cc153ad9
Hyuncheol Kim <81357653+kimreeha@users.noreply.github.com>
2023-05-20 18:59:34 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,10 @@
+def solution(price):
+    if price >= 500000:
+        answer = int(price * 0.8)
+    elif price >= 300000:
+        answer = int(price * 0.9)
+    elif price >= 100000:
+        answer = int(price * 0.95)
+    else:
+        answer = int(price)
+    return answer
