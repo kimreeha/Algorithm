82ed9beeb75d604840e45c9f6609d016392e3030
Hyuncheol Kim <81357653+kimreeha@users.noreply.github.com>
2024-01-04 18:23:03 +0900

    [Silver IV] Title: 제로, Time: 3912 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,14 @@
+# 10773. 제로
+
+K = int(input())
+
+stack = []
+
+for i in range(K):
+    N = int(input())
+    if N != 0:
+        stack.append(N)
+    else:
+        stack.pop()
+
+print(sum(stack))
